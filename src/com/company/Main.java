e8dab671f39662d3f9d7e4a3fec14c8e95ac4297
emil.falkowski <efmaniiax@gmail.com>
2016-12-27 12:31:17 +0100

    sideBranch
    something else

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("something else");
+
+
+        System.out.println("something else entirely");
     }
 }
