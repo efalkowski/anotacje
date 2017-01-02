7f6ee4424677349d69db578a72a205a445dbb7fc
emil.falkowski <efmaniiax@gmail.com>
2017-01-02 23:37:46 +0100

    Poprawione do wglądu 2.0
    -Dodano metodę zwracającą klasy w wybranym pakiecie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import sun.rmi.runtime.Log;
+
+import java.io.File;
+import java.net.URL;
 import java.util.*;
 
 
@@ -12,21 +16,65 @@ public class Main {
            System.exit(1);
         }
 
-        List<Class> myList = Arrays.asList(Dog.class, Cat.class, Monkey.class);
+        Set<Class> myList = getClassesInPackage("com.company");
 
         for (Class cl : myList) {
-            Object instance = cl.newInstance();
 
-            if (instance instanceof Animal) {
-                MyAnnotation myAnnotation = cl.getMethod("eat").getAnnotation(MyAnnotation.class);
+            if(!cl.isInterface()) {
+               Object instance = cl.newInstance();
 
-                if (myAnnotation.anAnimal().equals(args[0])) {
-                   ((Animal) instance).eat();
-                   break;
+                if (instance instanceof Animal) {
+                    MyAnnotation myAnnotation = cl.getMethod("eat").getAnnotation(MyAnnotation.class);
+
+                    if (myAnnotation.anAnimal().equals(args[0])) {
+                        ((Animal) instance).eat();
+                        break;
+                    }
+
                 }
-
             }
-
         }
+    }
+    
+    private static Set<Class> getClassesInPackage(String packageName) {
+        Set<Class> classes = new HashSet<>();
+        String packageNameSlashed = packageName.replace(".", "/");
+        // Get a File object for the package
+        URL directoryURL = Thread.currentThread().getContextClassLoader().getResource(packageNameSlashed);
+
+        if (directoryURL == null) {
+            System.out.println("Could not retrieve URL resource: " + packageNameSlashed);
+            return classes;
+        }
+
+        String directoryString = directoryURL.getFile();
+
+        if (directoryString == null) {
+            System.out.println("Could not find directory for URL resource: " + packageNameSlashed);
+            return classes;
+        }
+
+        File directory = new File(directoryString);
+
+        if (directory.exists()) {
+            // Get the list of the files contained in the package
+            String[] files = directory.list();
+
+            for (String fileName : files){
+                // We are only interested in .class files
+                if (fileName.endsWith(".class")) {
+                    // Remove the .class extension
+                    fileName = fileName.substring(0, fileName.length() - 6);
+                    try {
+                        classes.add(Class.forName(packageName + "." + fileName));
+                    } catch (ClassNotFoundException e) {
+                        System.out.println(packageName + "." + fileName + " does not appear to be a valid class." + e);
+                    }
+                }
+            }
+        } else {
+            System.out.println(packageName + " does not appear to exist as a valid package on the file system.");
+        }
+        return classes;
     }
 }
